cf07a1475acc51aeea5d6a86686ad8349be982c9
Anthony Ngure <anthonyngure25@gmail.com>
2019-09-23 09:15:43 +0300

    - Added geo module and extensions module

--- app/src/main/java/ke/co/toshngure/androidcoreutils/MaterialActivity.kt
+++ app/src/main/java/ke/co/toshngure/androidcoreutils/MaterialActivity.kt
@@ -1,12 +1,10 @@
 package ke.co.toshngure.androidcoreutils
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MaterialActivity : AppCompatActivity() {
 
@@ -19,12 +17,12 @@ class MaterialActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
+        /*val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)*/
         navView.setupWithNavController(navController)
     }
 }
